c3e813ca95632588eece99a963831008a7abe91b
jaelyntran <70259055+jaelyntran@users.noreply.github.com>
2025-01-24 20:14:54 -0800

    Finish part 1 of the assignment: update earliest and latest tweets date, count events by their categories, and count numbers of completed events with written text

--- js/about.js
+++ js/about.js
@@ -5,13 +5,71 @@ function parseTweets(runkeeper_tweets) {
 		return;
 	}
 
-	tweet_array = runkeeper_tweets.map(function(tweet) {
+	let tweet_array = runkeeper_tweets.map(function(tweet) {
 		return new Tweet(tweet.text, tweet.created_at);
 	});
-	
-	//This line modifies the DOM, searching for the tag with the numberTweets ID and updating the text.
-	//It works correctly, your task is to update the text of the other tags in the HTML file!
-	document.getElementById('numberTweets').innerText = tweet_array.length;	
+
+	let earliest_tweet = tweet_array.reduce((earliest, current) => {
+		return new Date(current.time) < new Date(earliest.time) ? current : earliest;
+	});
+	let latest_tweet = tweet_array.reduce((latest, current) => {
+		return new Date(current.time) > new Date(latest.time) ? current : latest;
+	});
+
+	let completed_events= 0;
+	let live_events= 0;
+	let achievements= 0;
+	let miscellaneous= 0;
+
+	tweet_array.forEach(tweet => {
+		switch(tweet.source) {
+			case "completed_event": completed_events++; break;
+			case "live_event": live_events++; break;
+			case "achievement": achievements++; break;
+			case "miscellaneous": miscellaneous++; break;
+		}
+	});
+	let completedEventsPct = math.format(completed_events / tweet_array.length * 100, { notation: 'fixed', precision: 2 });
+	let liveEventsPct = math.format(live_events / tweet_array.length * 100, { notation: 'fixed', precision: 2 });
+	let achievementsPct = math.format(achievements / tweet_array.length * 100, { notation: 'fixed', precision: 2 });
+	let miscPct = math.format(miscellaneous / tweet_array.length * 100, { notation: 'fixed', precision: 2 });
+
+	let userWrittenCount = 0;
+	tweet_array.forEach(tweet => {
+		if(tweet.written)
+		{
+			userWrittenCount++;
+			console.log(tweet.writtenText);
+		}
+	})
+	let userWrittenPct = math.format(userWrittenCount / tweet_array.length * 100, { notation: 'fixed', precision: 2 });
+
+	console.log("Updating numberTweets:", tweet_array.length);
+	document.getElementById('numberTweets').innerText = tweet_array.length;
+
+	const dateOptions = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
+	console.log("Updating firstDate:", earliest_tweet.time);
+	document.getElementById('firstDate').innerText = earliest_tweet.time.toLocaleDateString(
+		'en-US', dateOptions);
+	console.log("Updating lastDate:", latest_tweet.time);
+	document.getElementById('lastDate').innerText = latest_tweet.time.toLocaleDateString(
+		'en-US', dateOptions);
+
+	console.log("Updating tweet categories & their percentages:", completed_events, live_events, achievements, miscellaneous);
+	document.querySelectorAll('.completedEvents').forEach(function (element) {
+		element.innerText = completed_events.toString()
+	});
+	document.querySelector('.completedEventsPct').innerText = completedEventsPct.toString() + "%";
+	document.querySelector('.liveEvents').innerText = live_events.toString();
+	document.querySelector('.liveEventsPct').innerText = liveEventsPct.toString() + "%";
+	document.querySelector('.achievements').innerText = achievements.toString();
+	document.querySelector('.achievementsPct').innerText = achievementsPct.toString() + "%";
+	document.querySelector('.miscellaneous').innerText = miscellaneous.toString();
+	document.querySelector('.miscellaneousPct').innerText = miscPct.toString() + "%";
+
+	console.log("Updating user written text count and percentage");
+	document.querySelector('.written').innerText = userWrittenCount.toString();
+	document.querySelector('.writtenPct').innerText = userWrittenPct.toString() + "%";
 }
 
 //Wait for the DOM to load
--- ts/tweet.ts
+++ ts/tweet.ts
@@ -9,13 +9,34 @@ class Tweet {
 
 	//returns either 'live_event', 'achievement', 'completed_event', or 'miscellaneous'
     get source():string {
-        //TODO: identify whether the source is a live event, an achievement, a completed event, or miscellaneous.
-        return "unknown";
+        //Identify whether the source is a live event, an achievement, a completed event, or miscellaneous.
+        const curr_text = this.text.toLowerCase();
+
+        if(curr_text.startsWith("just completed") || curr_text.startsWith("just posted")) {
+            return "completed_event";
+        }
+        else if(curr_text.includes("watch") && curr_text.includes("live")) {
+            return "live_event";
+        }
+        else if(curr_text.startsWith("achieved") && curr_text.includes("new personal record")) {
+            return "achievement"
+        }
+        else {
+            return "miscellaneous";
+        }
     }
 
     //returns a boolean, whether the text includes any content written by the person tweeting.
     get written():boolean {
         //TODO: identify whether the tweet is written
+        const curr_text = this.text.toLowerCase();
+
+        if(curr_text.startsWith("just completed") || curr_text.startsWith("just posted")) {
+            if(curr_text.indexOf("-") != -1)
+            {
+                return true;
+            }
+        }
         return false;
     }
 
@@ -24,7 +45,12 @@ class Tweet {
             return "";
         }
         //TODO: parse the written text from the tweet
-        return "";
+        else
+        {
+            let startIdx: number = this.text.indexOf("-") + 2;
+            let endIdx: number = this.text.indexOf("https");
+            return this.text.substring(startIdx, endIdx);
+        }
     }
 
     get activityType():string {
